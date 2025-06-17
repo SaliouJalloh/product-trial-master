83062173a2e21d87b4c5e44f7e61179c995b02b2
Your Name <your.email@example.com>
2025-06-17 23:37:50 +0200

    automatise mvn spotless:apply dans le ci

--- back/ecommerce-backend/src/test/java/com/alten/ecommerce/storage/persistence/UserPersistenceServiceTest.java
+++ back/ecommerce-backend/src/test/java/com/alten/ecommerce/storage/persistence/UserPersistenceServiceTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+
 import com.alten.ecommerce.exception.ResourceNotFoundException;
 import com.alten.ecommerce.storage.mapper.IUserPersistenceMapperImpl;
 import com.alten.ecommerce.storage.model.User;
